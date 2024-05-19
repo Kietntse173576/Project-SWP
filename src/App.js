be6e7e597040e316613a6dd518735f8ee639949a
Kietntse173576 <boibitcute@gmail.com>
2024-05-20 00:59:00 +0700

    add fe

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import Checkout from './components/Checkout';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import PaymentMethod from './components/PaymentMethod';
+import PaymentSuccess from './components/PaymentSuccess';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/payment-method" element={<PaymentMethod />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/product-detail/:productId" element={<ProductDetail />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
